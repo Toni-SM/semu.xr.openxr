322b6c37276e568cbd09ca15b2f58400db797579
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-03-31 19:34:53 +0200

    Rename scripts

---
+++ xr/pybind11_ext.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import glob
+from distutils.core import setup
+
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+# OV python (kit\python\include)
+if sys.platform == 'win32':
+    python_library_dir = os.path.join(os.path.dirname(sys.executable), "include")
+elif sys.platform == 'linux':
+    python_library_dir = os.path.join(os.path.dirname(sys.executable), "..", "lib")
+
+if not os.path.exists(python_library_dir):
+    raise Exception("OV Python library directory not found: {}".format(python_library_dir))
+
+
+ext_modules = [
+    Pybind11Extension("xrlib_p",
+                      glob.glob('src/*.cpp'),
+                      include_dirs=[os.path.join(os.getcwd(), "thirdparty", "openxr", "include"),
+                                    os.path.join(os.getcwd(), "thirdparty", "opengl", "include"),
+                                    os.path.join(os.getcwd(), "thirdparty", "sdl2")],
+                      library_dirs=[os.path.join(os.getcwd(), "thirdparty", "openxr", "lib"),
+                                    os.path.join(os.getcwd(), "thirdparty", "opengl", "lib"),
+                                    os.path.join(os.getcwd(), "thirdparty", "sdl2", "lib"),
+                                    python_library_dir],
+                      libraries=["openxr_loader", "GL", "SDL2"],
+                      extra_link_args=["-Wl,-rpath=./bin"],
+                      undef_macros=["CTYPES", "APPLICATION"],
+                      extra_compile_args=["-DXR_USE_GRAPHICS_API_OPENGL"],
+                     ),
+]
+
+setup(
+    name = 'openxr-lib',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules
+)
