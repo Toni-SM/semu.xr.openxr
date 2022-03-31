31acb98a9352416a21ba006f844cd3160fed818b
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-03-31 19:48:11 +0200

    Compile pybind11 using the local python environment

--- xr/compile_pybind11.bash
+++ xr/compile_pybind11.bash
@@ -4,7 +4,7 @@
 . clean_compiled_files.bash
 
 # compile code
-~/.local/share/ov/pkg/code-2022.1.0/kit/python/bin/python3 pybind11_ext.py build_ext --inplace
+python pybind11_ext.py build_ext --inplace
 
 # copy compiled file
 cp xrlib_p* ../bin/xrlib_p.so
